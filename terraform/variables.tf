ecfd875ca0457ae97c7ea5997f27e6f6b9e747fb
Deekshitha <puppaladeekshitha2@gmail.com>
2026-05-06 19:21:36 +0530

    Updated the names

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
-  default     = "portfolio-terraform-rg"
+  default     = "portfolio-terraform-rg-1922"
 }
 
 variable "location" {
@@ -10,22 +10,22 @@ variable "location" {
 
 variable "storage_account_name" {
   description = "Name of storage account for terraform state"
-  default     = "tfstateportfolio"
+  default     = "tfstateportfolio1922"
 }
 
 variable "acr_name" {
   description = "Name of Azure Container Registry"
-  default     = "weappACR"
+  default     = "weappACR1922"
 }
 
 variable "app_service_plan_name" {
   description = "Name of App Service Plan"
-  default     = "ASP-devopsazure-87e7"
+  default     = "ASP-devopsazure-87e71922"
 }
 
 variable "web_app_name" {
   description = "Name of Web App"
-  default     = "deekshitha-webappresource"
+  default     = "deekshitha-webappresource1922"
 }
 
 variable "docker_image" {
