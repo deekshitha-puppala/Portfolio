2575a4e0d5eaeb0de957601e7f5e4f5d01567771
Deekshitha <puppaladeekshitha2@gmail.com>
2026-05-06 19:03:53 +0530

    Updated the storage account in providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "portfolio-terraform-rg"
-    storage_account_name = "tfstateportfolio"
+    storage_account_name = "tfstateportfolio2205"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
